ef4201c97d8e7eab2c042460c89ded826f982ef6
Ulash Yshyk <ulash@Ulashs-MacBook-Air-533.local>
2025-11-09 15:07:44 -0500

    Deleted welcome folder

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,16 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "AI Resume Analyzer" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track</h1>
+      </div>
+    </section>
 }
